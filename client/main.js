bbe7c45546933ad123d9a20c45c5117a1129c269
darlingdeau <32135875+darlingdeau@users.noreply.github.com>
2017-10-11 23:50:35 -0500

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -16,11 +16,40 @@ console.log(question+answer); //string & string
 var numberMessage = "The total numbere is:";
 var totalNum = 10;
 console.log(numberMessage+totalNum); //String & number
-var person = "Jessica";
+var person = "Serena";
 if(person === "Jessica")
 {
     console.log("Hello, Jessie!");
 }
 else {
   console.log("Who's this?");
+}
+var IQ = 179;
+if (IQ >= 180)
+{
+  console.log("Smartaf!")
+}
+else
+{
+  console.log("No te preocupes! Soy el mismo.")
+}
+
+for(age = 0; age <100; age++)
+{
+  if (age === 0)
+  {
+    console.log(age+":Welcome to this world!");
+  }
+  else if(age === 25)
+  {
+    console.log(age+":Young, and wild, and free");
+  }
+  else if (age === 75)
+  {
+    console.log(age+":So wise.");
+  }
+  else if (age === 99)
+  {
+    console.log(age+":Still so lively!");
+  }
 }
