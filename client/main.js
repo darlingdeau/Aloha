d6392de219dc0980109f53df1e1a3378d654f17f
darlingdeau <32135875+darlingdeau@users.noreply.github.com>
2017-10-11 23:31:34 -0500

    Add files via upload

---
+++ client/main.js
@@ -0,0 +1,26 @@
+var HelloMessage = "Hello,World!";
+  console.log (HelloMessage);
+var fruit = "blueberries";
+var fruitNum = "17";
+  console.log (fruit);
+  console.log (fruitNum);
+  HelloMessage = "What's up kiddo?";
+  console.log (HelloMessage);
+var studentNum = 7;
+var teacherNum = 1;
+var allPeopleNum = studentNum+teacherNum;
+console.log(allPeopleNum);
+var question = "What's your name?";
+var answer = " Sydney";
+console.log(question+answer); //string & string
+var numberMessage = "The total numbere is:";
+var totalNum = 10;
+console.log(numberMessage+totalNum); //String & number
+var person = "Jessica";
+if(person === "Jessica")
+{
+    console.log("Hello, Jessie!");
+}
+else {
+  console.log("Who's this?");
+}
---
+++ server/main.js
@@ -0,0 +1,3 @@
+var numbers = [1, 2, 3, 4, 5];
+            // 0  1  2  3  4
+console.log(numbers[0])
